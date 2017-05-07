8059316d69d237b77b157c5708031b2624c9fb20
Adam Newgas <boris@boristhebrave.com>
2017-05-07 21:44:07 +0100

    Working proof of concept

--- blue_noise_particles.py
+++ blue_noise_particles.py
@@ -1,3 +1,9 @@
+import bpy
+import math
+from mathutils.kdtree import KDTree
+from functools import total_ordering
+import heapq
+
 bl_info = {
     "name": "Blue Noise Particles",
     "description": "",
@@ -9,9 +15,80 @@ bl_info = {
     "wiki_url": "",
     "category": ""}
 
-import bpy
-import bmesh
-from mathutils.kdtree import KDTree
+
+@total_ordering
+class HeapItem:
+    def __init__(self, weight, index):
+        self.weight = weight
+        self.index = index
+
+    def __lt__(self, other):
+        return self.weight > other.weight
+
+class SampleEliminator:
+    def __init__(self, locations, target_samples):
+        self.locations = locations
+        self.tree = KDTree(len(locations))
+        for index, location in locations.items():
+            self.tree.insert(location, index)
+        self.tree.balance()
+        self.rmax = 0.1  # TODO: Choose this appropriately
+        self.alpha = 8
+        self.target_samples = target_samples
+        self.current_samples = len(self.locations)
+
+        gamma = 1.5
+        beta = 0.65
+        M = self.current_samples
+        N = self.target_samples
+        self.rmin = self.rmax * (1 - (N / M) ** gamma) * beta
+
+        # Build initial heap
+        self.heap = []
+        self.heap_items = {}
+        for index, location in locations.items():
+            tot_weight = 0
+            for location2, index2, d in self.tree.find_range(location, 2 * self.rmax):
+                tot_weight += self.w(d)
+            item = HeapItem(tot_weight, index)
+            self.heap_items[index] = item
+            heapq.heappush(self.heap, item)
+
+    def eliminate_one(self):
+        item = heapq.heappop(self.heap)
+        index = item.index
+        print("eliminating", index)
+        location = self.locations[index]
+        for location2, index2, d in self.tree.find_range(location, 2 * self.rmax):
+            item2 = self.heap_items[index2]
+            item2.weight -= self.w(d)
+        heapq.heapify(self.heap)
+        self.current_samples -= 1
+
+    def eliminate(self):
+        print("elimiate")
+        print(self.current_samples)
+        print(self.target_samples)
+        while self.current_samples > self.target_samples:
+            self.eliminate_one()
+
+    def get_indices(self):
+        return (item.index for item in self.heap)
+
+    def d(self, i, j):
+        li = self.locations[i]
+        lj = self.locations[j]
+        return math.sqrt((li[0] - lj[0]) ** 2 +
+                         (li[1] - lj[1]) ** 2 +
+                         (li[2] - lj[2]) ** 2)
+
+    def adj_d(self, d):
+        return min(d, 2 * self.rmax)
+
+    def w(self, d):
+        return (1 - self.adj_d(d) / 2 / self.rmax) ** self.alpha
+
+
 
 
 class BlueNoiseParticles(bpy.types.Operator):
@@ -34,27 +111,32 @@ class BlueNoiseParticles(bpy.types.Operator):
         bpy.ops.object.particle_system_add()
         psys = obj.particle_systems[-1]  # type: bpy.types.ParticleSystem
         pset = psys.settings
-        pset.count = 2000
+        #pset.count = 10000
         pset.show_unborn = True
 
         particles = psys.particles
 
-        tree = KDTree(len(particles))
+        locations = dict((index, particle.location) for (index, particle) in particles.items())
+        se = SampleEliminator(locations, 500)
+        se.eliminate()
+
+        alive_indices = set(se.get_indices())
+        print(alive_indices)
 
         for index, particle in particles.items():
-            tree.insert(particle.location, index)
-
-        tree.balance()
-
-        co, index, dist = tree.find([0,0,0])
-        print(co)
+            alive = index in alive_indices
+            particle.alive_state = 'ALIVE' if alive else 'DEAD'
+            if not alive:
+                particle.location = [0,0,0]
 
         return {'FINISHED'}
+
 
 def menu_func(self, context):
     self.layout.operator(BlueNoiseParticles.bl_idname,
                          text="Blue Noise Particles",
                          icon='PLUGIN')
+
 
 def register():
     bpy.utils.register_module(__name__)
